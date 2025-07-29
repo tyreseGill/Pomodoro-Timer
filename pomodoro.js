0bbd808a0336b7abce41c7b15b76a83108d393f6
Ty <168314093+tyreseGill@users.noreply.github.com>
2025-07-29 11:19:51 -0700

    refactor: move TODO comments to designated file

--- pomodoro.js
+++ pomodoro.js
@@ -10,7 +10,6 @@ const staticTvScreen = document.querySelector("#static-tv-screen");
 const videoTvScreen = document.querySelector("#video-screen");
 const searchBar = document.querySelector("input[type='search']");
 
-// TODO: Setting same time for each distinct state prevents change of state due to equality
 const STATE = {
     "STUDYING": "25:00",
     "SHORT_BREAK": "5:00",
@@ -224,9 +223,7 @@ addMusicBtn.addEventListener('click', () => {
         thumbnailLink = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`
 
         musicToBePlayed.push(embedLink);
-        // localStorage.setItem("")
 
-        // TODO: Throw error
         if (document.querySelector("#no-video-text")){
             placeholder.remove();
         }
@@ -276,43 +273,5 @@ staticTvScreen.addEventListener("click", () => {
     toggleTV(staticTvScreen);
     toggleMute();
 });
-
-
-// TODO: Add Cache to local storage to prevent copy/paste repeatedly
-// TODO: Add YouTube Video API
-// TODO: Move video element down and maybe zoom in more. Video currently has empty space on top and bottom
-// TODO: Have user make two playlists: one listing videos to play/watch during relaxation breaks and one to have playing during actual study sessions
-// TODO: Currently pushing entire embed link when should be pushing just video ID to save space
-
-
-// function addMealLS(mealId){
-//     const mealIds = getMealsLS();
-//     localStorage.setItem("mealIds", JSON.stringify(
-//         [...mealIds, mealId]));
-// }
-
-// function removeMealLS(mealId){
-//     const mealIds = getMealsLS();
-
-//     localStorage.setItem("mealIds", JSON.stringify(
-//         mealIds.filter(id => id !== mealId)
-//     ));
-// }
-
-// function getMealsLS(){
-//     const mealIds = JSON.parse(localStorage.getItem("mealIds"));
-//     return mealIds === null ? [] : mealIds;
-// }
-
-// window.addEventListener("DOMContentLoaded", () => {
-//     const savedSearch = localStorage.getItem("searchBar");
-//     if (savedSearch){
-//         searchBar.value = savedSearch;
-//     }
-// });
-
-// searchBar.addEventListener("input", () => {
-//     localStorage.setItem(savedSearch, searchBar.value);
-// });
 
 // videoTvScreen.addEventListener("click", toggleTV);
